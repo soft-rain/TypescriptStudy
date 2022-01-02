0920055905a09eca24d9ddd9e39213b68835c54a
softrain <yurimplus@naver.com>
2022-01-03 01:56:09 +0900

    [#1] 타입 추론

---
+++ Typescript - 땅콩코딩/app.ts
@@ -0,0 +1,11 @@
+let student = {
+    name:'Jake',
+    course :'Getting started with Typescript',
+    codingIQ:80,
+    code:function(){
+        console.log('brain is working hard');
+    }
+}
+function calculateCodingIQ(lostPoints){
+    return 100 - lostPoints;
+}
