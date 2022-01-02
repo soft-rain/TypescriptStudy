09f059131057b915be336e09779d28da20da9659
softrain <yurimplus@naver.com>
2022-01-03 02:12:57 +0900

    [#1] 타입 명시

--- Typescript - 땅콩코딩/app.js
+++ Typescript - 땅콩코딩/app.js
@@ -1,4 +1,9 @@
-function logName(name) {
-    console.log(name);
+var studentID = 12345;
+var studentName = "jenny Kim";
+var age = 23;
+var gender = "female";
+var subject = "javascript";
+var courseCompleted = false;
+function getStudentDetails(studentID) {
+    return null;
 }
-logName('Jack');
--- Typescript - 땅콩코딩/app.ts
+++ Typescript - 땅콩코딩/app.ts
@@ -1,11 +1,18 @@
-let student = {
-    name:'Jake',
-    course :'Getting started with Typescript',
-    codingIQ:80,
-    code:function(){
-        console.log('brain is working hard');
-    }
-}
-function calculateCodingIQ(lostPoints){
-    return 100 - lostPoints;
+let studentID:number  =12345;
+let studentName:string= "jenny Kim";
+let age:number = 23;
+let gender:string = "female";
+let subject:string = "javascript";
+let courseCompleted:boolean = false;
+
+function getStudentDetails(studentID:number)
+:{
+    studentID:number;
+    studentName:string;
+    age:number;
+    gender:string;
+    subject:string;
+    createDate:Date;
+} {
+    return null;
 }
