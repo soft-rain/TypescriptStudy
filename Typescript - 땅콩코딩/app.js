60c52b512854d86adfc793514112fe26d26d6e7f
softrain <yurimplus@naver.com>
2022-01-03 02:28:07 +0900

    [#2] 인터페이스

--- Typescript - 땅콩코딩/app.js
+++ Typescript - 땅콩코딩/app.js
@@ -4,6 +4,23 @@ var age = 23;
 var gender = "female";
 var subject = "javascript";
 var courseCompleted = false;
+var student1 = {
+    studentID: 12123123,
+    studentName: "janet jackson",
+    age: 30,
+    gender: "female",
+    subject: "mongo db",
+    courseCompleted: true
+};
 function getStudentDetails(studentID) {
-    return null;
+    return {
+        studentID: 123456,
+        studentName: 'Mark jacobs',
+        gender: 'male',
+        subject: 'Node.js',
+        courseCompleted: true
+    };
 }
+function saveStudentDetails(student) {
+}
+saveStudentDetails(student1);
--- Typescript - 땅콩코딩/app.ts
+++ Typescript - 땅콩코딩/app.ts
@@ -5,14 +5,40 @@ let gender:string = "female";
 let subject:string = "javascript";
 let courseCompleted:boolean = false;
 
-function getStudentDetails(studentID:number)
-:{
-    studentID:number;
+//인터페이스
+interface Student { 
+    readonly studentID:number;
     studentName:string;
-    age:number;
+    age?:number;
     gender:string;
     subject:string;
-    createDate:Date;
-} {
-    return null;
+    courseCompleted:boolean;
+    //메소드
+    //addComment(comment:string):string;
+    addComment?:(comment:string)=>string;
 }
+
+let student1 = {
+    studentID:12123123,
+    studentName:"janet jackson",
+    age:30,
+    gender:"female",
+    subject:"mongo db",
+    courseCompleted:true
+}
+
+function getStudentDetails(studentID:number)
+:Student {
+    return {
+        studentID:123456,
+        studentName:'Mark jacobs',
+        gender:'male',
+        subject:'Node.js',
+        courseCompleted:true
+        
+    };
+}
+function saveStudentDetails(student:Student):void{
+
+}
+saveStudentDetails(student1)
