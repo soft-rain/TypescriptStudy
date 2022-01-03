0914d17b5882c34582d454dbccf078c0d20bc535
softrain <yurimplus@naver.com>
2022-01-03 20:58:14 +0900

    [#7] 생성자

--- Typescript - 땅콩코딩/app.js
+++ Typescript - 땅콩코딩/app.js
@@ -1,16 +1,15 @@
 var Employee = /** @class */ (function () {
-    function Employee() {
+    function Employee(fullName, age, jobTitle, hourlyRate, workingHoursPerWeek) {
         var _this = this;
         this.printEmployeeDetails = function () {
             console.log("".concat(_this.fullName, "\uC758 \uC9C1\uC5C5\uC740 ").concat(_this.jobTitle, "\uC774\uACE0 \uC77C\uC8FC\uC77C\uC758 \uC218\uC785\uC740 \n        ").concat(_this.hourlyRate * _this.workingHoursPerWeek, "\uB2EC\uB7EC\uC774\uB2E4."));
         };
+        this.fullName = fullName;
+        this.age = age;
+        this.jobTitle = jobTitle;
+        this.hourlyRate = hourlyRate;
+        this.workingHoursPerWeek = workingHoursPerWeek;
     }
     return Employee;
 }());
-var employee1 = new Employee();
-employee1.fullName = '민수';
-employee1.age = 28;
-employee1.jobTitle = "주니어 웹개발자";
-employee1.hourlyRate = 40;
-employee1.workingHoursPerWeek = 35;
-employee1.printEmployeeDetails();
+var employee1 = new Employee('민수', 28, '주니어 웹개발자', 40, 35);
--- Typescript - 땅콩코딩/app.ts
+++ Typescript - 땅콩코딩/app.ts
@@ -4,17 +4,20 @@ class Employee{
     jobTitle:string;
     hourlyRate:number;
     workingHoursPerWeek:number;
+
+    constructor(fullName:string, age:number,jobTitle:string, hourlyRate:number, workingHoursPerWeek:number){
+        this.fullName=fullName;
+        this.age=age;
+        this.jobTitle=jobTitle;
+        this.hourlyRate=hourlyRate;
+        this.workingHoursPerWeek=workingHoursPerWeek;
+
+
+    }
     printEmployeeDetails= ():void=>{
         console.log(`${this.fullName}의 직업은 ${this.jobTitle}이고 일주일의 수입은 
         ${this.hourlyRate*this.workingHoursPerWeek}달러이다.`)
     }
 }
  
-let employee1 = new Employee();
-
-employee1.fullName='민수';
-employee1.age=28;
-employee1.jobTitle="주니어 웹개발자";
-employee1.hourlyRate=40;
-employee1.workingHoursPerWeek=35;
-employee1.printEmployeeDetails();
+let employee1 = new Employee('민수', 28, '주니어 웹개발자', 40, 35);
