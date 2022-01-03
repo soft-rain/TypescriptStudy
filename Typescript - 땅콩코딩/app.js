bf586b77c7bba807e7ddfbfaca3d17a50d45b834
softrain <yurimplus@naver.com>
2022-01-03 20:31:19 +0900

    [#4] Any, Union타입, 타입가드

--- Typescript - 땅콩코딩/app.js
+++ Typescript - 땅콩코딩/app.js
@@ -1,31 +1,10 @@
-var studentID = 12345;
-var studentName = "jenny Kim";
-var age = 23;
-var gender = "Female";
-var subject = "javascript";
-var courseCompleted = false;
-var GenderType;
-(function (GenderType) {
-    GenderType["Male"] = "male";
-    GenderType["Female"] = "female";
-})(GenderType || (GenderType = {}));
-var student1 = {
-    studentID: 12123123,
-    studentName: "janet jackson",
-    age: 30,
-    gender: 'female',
-    subject: "mongo db",
-    courseCompleted: true
+var itemPrice;
+var setItemPrice = function (price) {
+    if (typeof price === 'string') {
+        itemPrice = 0;
+    }
+    else {
+        itemPrice = price;
+    }
 };
-function getStudentDetails(studentID) {
-    return {
-        studentID: 123456,
-        studentName: 'Mark jacobs',
-        gender: GenderType.Male,
-        subject: 'Node.js',
-        courseCompleted: true
-    };
-}
-function saveStudentDetails(student) {
-}
-saveStudentDetails(student1);
+setItemPrice(50);
--- Typescript - 땅콩코딩/app.ts
+++ Typescript - 땅콩코딩/app.ts
@@ -1,49 +1,11 @@
-let studentID:number  =12345;
-let studentName:string= "jenny Kim";
-let age:number = 23;
-let gender:string = "Female";
-let subject:string = "javascript";
-let courseCompleted:boolean = false;
+type StringOrNum = string | number;
+let itemPrice :  number;
 
-enum GenderType{
-    Male = 'male',
-    Female='female'
+const setItemPrice=(price:StringOrNum):void=>{
+    if (typeof price === 'string'){
+        itemPrice=0;
+    } else{   
+        itemPrice=price;
+    }
 }
-
-//인터페이스
-interface Student { 
-    readonly studentID:number;
-    studentName:string;
-    age?:number;
-    gender:'male' | 'female';
-    subject:string;
-    courseCompleted:boolean;
-    //메소드
-    //addComment(comment:string):string;
-    addComment?:(comment:string)=>string;
-}
-
-let student1 = {
-    studentID:12123123,
-    studentName:"janet jackson",
-    age:30,
-    gender:'female',
-    subject:"mongo db",
-    courseCompleted:true
-}
-
-function getStudentDetails(studentID:number)
-:Student {
-    return {
-        studentID:123456,
-        studentName:'Mark jacobs',
-        gender:GenderType.Male,
-        subject:'Node.js',
-        courseCompleted:true
-        
-    };
-}
-function saveStudentDetails(student:Student):void{
-
-}
-saveStudentDetails(student1)
+setItemPrice(50);
