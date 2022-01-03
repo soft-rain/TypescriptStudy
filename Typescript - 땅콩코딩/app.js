bf9875b3e26b84227d887bd36e6b60f906629855
softrain <yurimplus@naver.com>
2022-01-03 20:49:06 +0900

    [#6] 객체지향프로그래밍 - 클래스, 오브젝트

--- Typescript - 땅콩코딩/app.js
+++ Typescript - 땅콩코딩/app.js
@@ -1,8 +1,16 @@
-function sendGreeting(message, userName) {
-    if (message === void 0) { message = 'Hello'; }
-    if (userName === void 0) { userName = 'there'; }
-    console.log("".concat(message, ", ").concat(userName));
-}
-sendGreeting();
-sendGreeting('Good morning');
-sendGreeting('Good afternoon', 'Jenny');
+var Employee = /** @class */ (function () {
+    function Employee() {
+        var _this = this;
+        this.printEmployeeDetails = function () {
+            console.log("".concat(_this.fullName, "\uC758 \uC9C1\uC5C5\uC740 ").concat(_this.jobTitle, "\uC774\uACE0 \uC77C\uC8FC\uC77C\uC758 \uC218\uC785\uC740 \n        ").concat(_this.hourlyRate * _this.workingHoursPerWeek, "\uB2EC\uB7EC\uC774\uB2E4."));
+        };
+    }
+    return Employee;
+}());
+var employee1 = new Employee();
+employee1.fullName = '민수';
+employee1.age = 28;
+employee1.jobTitle = "주니어 웹개발자";
+employee1.hourlyRate = 40;
+employee1.workingHoursPerWeek = 35;
+employee1.printEmployeeDetails();
--- Typescript - 땅콩코딩/app.ts
+++ Typescript - 땅콩코딩/app.ts
@@ -1,6 +1,20 @@
-function sendGreeting(message = 'Hello', userName = 'there'):void{
-    console.log(`${message}, ${userName}`);
+class Employee{
+    fullName:string;
+    age :number;
+    jobTitle:string;
+    hourlyRate:number;
+    workingHoursPerWeek:number;
+    printEmployeeDetails= ():void=>{
+        console.log(`${this.fullName}의 직업은 ${this.jobTitle}이고 일주일의 수입은 
+        ${this.hourlyRate*this.workingHoursPerWeek}달러이다.`)
     }
-    sendGreeting();
-    sendGreeting('Good morning');
-    sendGreeting('Good afternoon', 'Jenny');
+}
+ 
+let employee1 = new Employee();
+
+employee1.fullName='민수';
+employee1.age=28;
+employee1.jobTitle="주니어 웹개발자";
+employee1.hourlyRate=40;
+employee1.workingHoursPerWeek=35;
+employee1.printEmployeeDetails();
